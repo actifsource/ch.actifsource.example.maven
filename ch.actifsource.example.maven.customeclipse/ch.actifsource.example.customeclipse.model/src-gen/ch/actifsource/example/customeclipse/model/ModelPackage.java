f07b2649f726956676a94988782d7928db5104e1
ruti <grischa.rutishauser@actifsource.com>
2022-05-18 14:43:04 +0200

    add actifsource custom eclipse

---
+++ ch.actifsource.example.maven.customeclipse/ch.actifsource.example.customeclipse.model/src-gen/ch/actifsource/example/customeclipse/model/ModelPackage.java
@@ -0,0 +1,31 @@
+package ch.actifsource.example.customeclipse.model;
+
+import ch.actifsource.core.Resource;
+
+public class ModelPackage {
+  
+  public static final Resource TestModelClass = new Resource("073519ee-d69e-11ec-9669-c7063ed32e69");
+  public static final Resource SubClass = new Resource("0aafe8dd-d69e-11ec-9669-c7063ed32e69");
+  public static final Resource TestModelClass_subClass = new Resource("0d70687c-d69e-11ec-9669-c7063ed32e69");
+  public static final Resource TestFunctions_SubClass = new Resource("be203da0-d6a3-11ec-9669-c7063ed32e69");
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private ModelPackage() {}
+  
+  public static final String getScopeName() {
+    return "ch.actifsource.example.customeclipse.model";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "ch.actifsource.example.customeclipse.model";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,ch.actifsource.example.customeclipse.model,G4w6/y56mLHdU7Mn6Mc1qgRtz7c=] */
