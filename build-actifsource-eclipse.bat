678599e3c1151916ce67d5379aeb99c9d036987b
ruti <grischa.rutishauser@actifsource.com>
2023-02-27 17:25:06 +0100

    Add script to build actifsource eclipse

---
+++ build-actifsource-eclipse.bat
@@ -0,0 +1,23 @@
+@echo off
+echo Downloading Maven...
+set MAVEN_VERSION=3.8.4
+set MAVEN_BASE_URL=https://dlcdn.apache.org/maven/maven-3/3.9.0/binaries/
+set MAVEN_ARCHIVE_NAME=apache-maven-3.9.0-bin.zip
+set MAVEN_DOWNLOAD_URL=%MAVEN_BASE_URL%/%MAVEN_ARCHIVE_NAME%
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%MAVEN_DOWNLOAD_URL%', '%MAVEN_ARCHIVE_NAME%')"
+echo Extracting Maven...
+powershell Expand-Archive %MAVEN_ARCHIVE_NAME% -DestinationPath .\
+echo Cleaning up...
+del %MAVEN_ARCHIVE_NAME%
+set MAVEN_DOWNLOAD_URL=%MAVEN_BASE_URL%/%MAVEN_ARCHIVE_NAME%
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%MAVEN_DOWNLOAD_URL%', '%MAVEN_ARCHIVE_NAME%')"
+
+echo Clone Maven Project...
+git clone https://github.com/actifsource/ch.actifsource.example.maven.git
+
+echo Build Project...
+REM set JAVA_HOME=jre
+echo JAVA_HOME: %JAVA_HOME%
+apache-maven-3.9.0/bin/mvn clean install -f ch.actifsource.example.maven/ch.actifsource.example.maven.actifsourceeclipse/ch.actifsource.example.actifsourceeclipse/ 
+echo Eclipse Location: 'ch.actifsource.example.maven\ch.actifsource.example.maven.actifsourceeclipse\ch.actifsource.example.actifsourceeclipse.product\target\products' 
+echo Done.
